efa4fe622f8838ed1baa4a3b3b274a2589d8cc7d
pjosed <pjosed@uninorte.edu.co>
2024-11-20 17:00:01 -0500

    Controladores actualizados

--- src/core/controllers/CreateAccountController.java
+++ src/core/controllers/CreateAccountController.java
@@ -4,10 +4,38 @@
  */
 package core.controllers;
 
+import core.controllers.utils.Response;
+import core.controllers.utils.Status;
+import core.models.bank.Account;
+import core.models.person.User;
+import core.models.storage.Storage;
+
 /**
  *
  * @author Usuario
  */
 public class CreateAccountController {
-    
+    public static Response createAccount(String id, User owner, int balance) {
+        try {
+            int idInt;
+            
+            try {
+                idInt = Integer.parseInt(id);
+                if (idInt < 0) {
+                    return new Response("Id must be positive", Status.BAD_REQUEST);
+                }
+            } catch (NumberFormatException ex) {
+                return new Response("Id must be numeric", Status.BAD_REQUEST);
+            }
+            
+            
+            Storage storage = Storage.getInstance();            
+            //if (!storage.addAccount(new Account(idInt, owner , balance))) {
+                //return new Response("A person with that id already exists", Status.BAD_REQUEST);
+            //}
+            return new Response("Person created successfully", Status.CREATED);
+        } catch (Exception ex) {
+            return new Response("Unexpected error", Status.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/core/models/storage/Storage.java
+++ src/core/models/storage/Storage.java
@@ -4,10 +4,72 @@
  */
 package core.models.storage;
 
+import core.models.bank.Account;
+import core.models.person.User;
+import java.util.ArrayList;
+
 /**
  *
  * @author Usuario
  */
 public class Storage {
+    
+    // Instancia Singleton
+    private static Storage instance;
+    
+    // Atributos del Storage
+    private ArrayList<User> users;
+    private ArrayList<Account> accounts;
+    
+    private Storage() {
+        this.users = new ArrayList<>();
+    }
+    
+    public static Storage getInstance() {
+        if (instance == null) {
+            instance = new Storage();
+        }
+        return instance;
+    }
+    
+    public boolean addUser(User user) {
+        for (User u : this.users) {
+            if (u.getId() == user.getId()) {
+                return false;
+            }
+        }
+        this.users.add(user);
+        return true;
+    }
+    
+        public boolean addAccount(Account account) {
+        for (Account a: this.accounts) {
+            if (a.getId().equals(account.getId())) { // Uso equals porque es String
+                return false;
+            }
+        }
+        this.accounts.add(account);
+        return true;
+    }
+    
+    
+    public User getUser(int id) {
+        for (User user : this.users) {
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+        return null;
+    }
+    
+    public boolean delUser(int id) {
+        for (User user : this.users) {
+            if (user.getId() == id) {
+                this.users.remove(user);
+                return true;
+            }
+        }
+        return false;
+    }
     
 }
